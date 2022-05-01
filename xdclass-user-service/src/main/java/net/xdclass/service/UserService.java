9b5fcf04ca4c9ff92a268ff8a4ad10546fa4de65
john <17717990@.com>
2022-05-01 18:04:48 +0800

    基础代码生成

---
+++ xdclass-user-service/src/main/java/net/xdclass/service/UserService.java
@@ -0,0 +1,16 @@
+package net.xdclass.service;
+
+import net.xdclass.model.UserDO;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author john
+ * @since 2022-05-01
+ */
+public interface UserService extends IService<UserDO> {
+
+}
